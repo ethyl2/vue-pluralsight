2d3d29eebb4c2122cefc82b99916b22969d2e7bb
Heather Nuffer <zzzuzz@gmail.com>
2020-10-21 20:28:15 -0600

    Adds use of environ vars and parsese data rec'd from api

--- 07-accessing-data/begin/vue-heroes/src/shared/data.js
+++ 07-accessing-data/begin/vue-heroes/src/shared/data.js
@@ -1,14 +1,28 @@
 import { format } from 'date-fns';
 import { inputDateFormat } from './constants';
 import axios from 'axios';
+import { API } from './config';
 
 const getHeroes = async function() {
-  const response = await axios.get('api/heroes.json');
-  const heroes = response.data.map(hero => {
-    hero.originDate = format(hero.originDate, inputDateFormat);
-    return hero;
-  });
-  return heroes;
+  try {
+    const response = await axios.get(`${API}/heroes.json`);
+    let data = parseList(response);
+    const heroes = data.map(hero => {
+      hero.originDate = format(hero.originDate, inputDateFormat);
+      return hero;
+    });
+    return heroes;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+const parseList = response => {
+  if (response.status !== 200) throw Error(response.message);
+  if (!response.data) return [];
+  let list = response.data;
+  return list;
 };
 
 const ourHeroesHardcoded = [
