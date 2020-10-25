b1e405ee7f8755067f769b2e984eea510ef25bce
Heather Nuffer <zzzuzz@gmail.com>
2020-10-24 23:51:20 -0600

    Feat: Adds nav side bar

---
+++ 08-routing/begin/vue-heroes/src/components/navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="column is-narrow menu">
+    <p class="menu-label">Menu</p>
+    <ul class="menu-list">
+      <router-link to="/heroes">Heroes</router-link>
+      <router-link to="/villains">Villains</router-link>
+      <router-link to="/about">About</router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
--- 08-routing/begin/vue-heroes/src/router.js
+++ 08-routing/begin/vue-heroes/src/router.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import Heroes from './views/heroes.vue';
-import About from './views/about.vue';
-// import Villains from './components/Villains';
+// import Heroes from './views/heroes.vue';
+// import HeroDetail from './views/hero-detail.vue';
+// import About from './views/about.vue';
+import Villains from './views/villains';
+import PageNotFound from './views/page-not-found';
 
 Vue.use(Router);
 
@@ -15,21 +17,32 @@ export default new Router({
       path: '/',
       redirect: '/heroes',
       name: 'heroesHome',
-      component: Heroes,
+      // component: Heroes,
+      component: () =>
+        import(/* webpackChunkName: "bundle-heroes" */ './views/heroes.vue'),
     },
     {
       path: '/heroes',
       name: 'heroes',
-      component: Heroes,
+      //component: Heroes,
+      component: () =>
+        import(/* webpackChunkName: "bundle-heroes" */ './views/heroes.vue'),
     },
-    /*
+    {
+      path: '/heroes/:id',
+      name: 'hero-detail',
+      //component: HeroDetail,
+      component: () =>
+        import(/* webpackChunkName: "bundle-heroes" */ './views/hero-detail.vue'),
+      // props: true // if you didn't need to parse the param
+      props: route => ({ id: parseInt(route.params.id) }),
+    },
     {
       path: '/villains',
       name: 'villains',
-      component: Villains,
+      component: Villains, // example of eager loading
     },
-    
-    */
+
     {
       path: '/about',
       name: 'about',
@@ -37,9 +50,13 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
 
-      //component: () =>
-      //  import(/* webpackChunkName: "about" */ './views/About.vue'),
-      component: About,
+      component: () =>
+        import(/* webpackChunkName: "bundle-about" */ './views/about.vue'),
+      //component: About,
+    },
+    {
+      path: '*',
+      component: PageNotFound,
     },
   ],
 });
